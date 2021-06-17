5093882ec083f3879e95ca7182b08472eaaa0a3d
Jakubinka <contact@jakubkowalski.dev>
2021-06-17 18:05:43 +0200

    🐱‍👤Added: Initial Single Product Page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
   <main-screen-vue2 />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import MainScreenVue2 from './components/MainScreenVue2/MainScreenVue2.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    MainScreenVue2
+    MainScreenVue2,
   }
 }
 </script>
--- src/components/MainScreenVue2/MainScreenVue2.vue
+++ src/components/MainScreenVue2/MainScreenVue2.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="hello">
-    <h3>Hello There {{ msg }}</h3>
+  <div class="product">
+    <div class="product__column">
+      <img class="product__image" src="../../assets/img/tesla-model-x-on-road.jpeg" alt="Picture of Tesla Model X" />
+    </div>
+    <div class="product__column">
+        <h1 class="product__heading">{{ productName }}</h1>
+        <p class="product__under-tag">{{ productUnderTag }}</p>
+        <div class="product__column-description-box">
+          <p class="product__description">{{ productDescription }}</p>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -8,10 +17,17 @@
 export default {
   name: 'MainScreenVue2',
   props: {
-    msg: String
+    productName: String,
+    productUnderTag: String,
+    productDescription: String,
   },
   data() {
-  }
+    return {
+      productName: 'Tesla Model X',
+      productUnderTag: 'Plaid',
+      productDescription: 'Long Range and Plaid platforms unite powertrain and battery technologies for unrivaled performance, range and efficiency. New module and pack thermal architecture allows faster charging and gives you more power and endurance in all conditions.',
+    }
+  },
 }
 </script>
 
