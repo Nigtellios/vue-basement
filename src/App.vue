13570a895d27703bd5d091591048d0b1ad50b685
Jakubinka <contact@jakubkowalski.dev>
2021-06-17 16:51:17 +0200

    🐱‍👤Finished: Configuration

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main-screen-vue2 />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MainScreenVue2 from './components/MainScreenVue2/MainScreenVue2.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MainScreenVue2
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MainScreenVue2/MainScreenVue2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h3>Hello There {{ msg }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainScreenVue2',
+  props: {
+    msg: String
+  },
+  data() {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" src="./MainScreenVue2.scss" />
