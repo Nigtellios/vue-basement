c25833f0ddfb6dee8dddc44f8d10a1ff0adee5ee
Jakubinka <contact@jakubkowalski.dev>
2021-06-23 23:06:21 +0200

    🐱‍👤Added: Product Highlight Section

--- src/components/SingleProductPageVue2/SingleProductPageVue2.vue
+++ src/components/SingleProductPageVue2/SingleProductPageVue2.vue
@@ -21,20 +21,24 @@
 
     <section class="product__features">
       <div class="product__features-item" v-for="feature in featuresData" :key="feature.featureID">
-        <img class="product__features-icon" :src="`${feature.iconURL}`" alt=""/>
+        <img class="product__features-icon" :src="`${feature.iconURL}`" :alt="`${feature.iconAlt}`"/>
         <div class="product__features-content">
-          <h4>{{ feature.featureName }}</h4>
+          <h4 class="product__features-heading">{{ feature.featureName }}</h4>
           <p>{{ feature.featureDescription }}</p>
         </div>
       </div>
     </section>
 
     <section class="product__highlights">
-      <div class="product__highlights-row">
-        <img class="product__highlights-image" src="" alt=""/>
-        <div class="product__highlights-content">
-          <h5 class="product__highlights-heading">Best Audio System</h5>
-          <p>A 22-speaker, 960-watt audio system with active noise canceling offers the best listening experience at home or on the road.</p>
+      <div class="product__highlights-row" v-for="highlight in highlightsData" :key="highlight.highlightID">
+        <div class="product__highlights-column">
+          <img class="product__highlights-image" :src="`${highlight.highlightImage}`" :alt="`${highlight.highlightImageAlt}`"/>
+        </div>
+        <div class="product__highlights-column">
+          <div class="product__highlights-content">
+            <h5 class="product__highlights-heading">{{ highlight.highlightTitle }}</h5>
+            <p class="product__highlights-p">{{ highlight.highlightDescription }}</p>
+          </div>
         </div>
       </div>
     </section>
@@ -61,27 +65,54 @@ export default {
           {
             "featureID": "0",
             "iconURL": "../../assets/icons/range.svg",
+            "iconAlt": "High range of vehicle",
             "featureName": "Range",
             "featureDescription": "340 Miles"
           },
           {
             "featureID": "1",
             "iconURL": "../../assets/icons/25smph.svg",
+            "iconAlt": "in 2.5s from 0 to 60 mph",
             "featureName": "2,5s",
             "featureDescription": "0-60 mph*"
           },
           {
             "featureID": "2",
             "iconURL": "../../assets/icons/14mile.svg",
+            "iconAlt": "in 9.9s at 1/4 Mile distance",
             "featureName": "9.9s",
             "featureDescription": "1/4 Mile"
           },
           {
             "featureID": "3",
             "iconURL": "../../assets/icons/peakpower.svg",
+            "iconAlt": "1020hp peak power",
             "featureName": "1,020hp",
             "featureDescription": "Peak Power"
           }
+      ],
+      highlightsData: [
+        {
+          "highlightID": "0",
+          "highlightImage": "../../assets/img/audio-system.jpg",
+          "highlightImageAlt": "Best audio system in the market",
+          "highlightTitle": "Best Audio System",
+          "highlightDescription": "A 22-speaker, 960-watt audio system with active noise canceling offers the best listening experience at home or on the road."
+        },
+        {
+          "highlightID": "1",
+          "highlightImage": "../../assets/img/multi-device.jpg",
+          "highlightImageAlt": "Stay connected with multiple devices",
+          "highlightTitle": "Stay Connected",
+          "highlightDescription": "Multi-device Bluetooth, wireless and USB-C charging for every passenger, with enough power to fast-charge your tablets and laptop."
+        },
+        {
+          "highlightID": "2",
+          "highlightImage": "../../assets/img/wireless-gaming.jpg",
+          "highlightImageAlt": "Let your kids play games while driving",
+          "highlightTitle": "Wireless Gaming",
+          "highlightDescription": "Up to 10 teraflops of processing power enables in-car gaming on-par with today’s newest consoles. Wireless controller compatibility lets you game from any seat."
+        }
       ]
     }
   },
