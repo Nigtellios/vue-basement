8c71a22b4d035e3f210d4ebddbc9d2971731592d
Jakubinka <contact@jakubkowalski.dev>
2021-07-09 14:13:01 +0200

    🐱‍👤Added: Navigation, Footer components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
+  <navigation />
   <single-product-page />
+  <main-footer />
 </template>
 
 <script>
@@ -8,10 +10,14 @@ import ProductInfo from './components/ProductInfo/ProductInfo.vue';
 import ProductFeatures from './components/ProductFeatures/ProductFeatures.vue';
 import ProductHighlights from './components/ProductHighlights/ProductHighlights.vue';
 import Sidebar from "@/components/Sidebar/Sidebar.vue";
+import Navigation from "@/components/Navigation/Navigation";
+import MainFooter from "@/components/MainFooter/MainFooter";
 
 export default {
   name: 'App',
   components: {
+    MainFooter,
+    Navigation,
     SingleProductPage,
     ProductInfo,
     ProductFeatures,
--- src/components/ProductHero/ProductHero.vue
+++ src/components/ProductHero/ProductHero.vue
@@ -72,36 +72,36 @@ export default {
   },
   data() {
     return {
-      // width: 1,
-      // height: 1,
-      // mousePosX: 0,
-      // mousePosY: 0,
+       /* width: 1,
+       height: 1,
+       mousePosX: 0,
+       mousePosY: 0, */
 
     }
   },
   mounted() {
-    // document.addEventListener('mousemove', this.parallaxMove);
+     // document.addEventListener('mousemove', this.parallaxMove);
   },
   destroyed() {
-    // document.removeEventListener('mousemove', this.parallaxMove)
+     // document.removeEventListener('mousemove', this.parallaxMove)
   },
   methods: {
 
     /* Old Parallax Function */
-    // parallaxMove(event) {
-    //   const element = document.querySelector("#parallax");
-    //
-    //   this.width = window.innerWidth/2;
-    //   this.height = window.innerHeight/2;
-    //   this.mousePosX = event.clientX;
-    //   this.mousePosY = event.clientY;
-    //
-    //   let firstLayer = `${30 - (this.mousePosX - this.width) * 0.05}% ${50 - (this.mousePosY - this.height) * 0.02}%`;
-    //   let secondLayer = `${50 - (this.mousePosX - this.width) * 0.02}% ${50 - (this.mousePosY - this.height) * 0.02}%`;
-    //   let thirdLayer = `${50 - (this.mousePosX - this.width) * 0.01}% ${140 - (this.mousePosY - this.height) * 0.01}%`;
-    //
-    //   element.style.backgroundPosition = `${thirdLayer}, ${secondLayer}, ${firstLayer}`;
-    // },
+     /*parallaxMove(event) {
+       const element = document.querySelector(".hero__box:after");
+
+       this.width = window.innerWidth/2;
+       this.height = window.innerHeight/2;
+       this.mousePosX = event.clientX;
+       this.mousePosY = event.clientY;
+
+       let firstLayer = `${30 - (this.mousePosX - this.width) * 0.05}% ${50 - (this.mousePosY - this.height) * 0.02}%`;
+       let secondLayer = `${50 - (this.mousePosX - this.width) * 0.02}% ${50 - (this.mousePosY - this.height) * 0.02}%`;
+       let thirdLayer = `${50 - (this.mousePosX - this.width) * 0.01}% ${140 - (this.mousePosY - this.height) * 0.01}%`;
+
+       element.style.backgroundPosition = `${thirdLayer}, ${secondLayer}, ${firstLayer}`;
+     },*/
   },
 }
 </script>
